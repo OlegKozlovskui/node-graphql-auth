e1552532586559b157607307458002fdfcb8f2c5
Oleh Kozlovskyi <okozlovskyi@n-ix.com>
2020-03-04 18:06:31 +0200

    init commit

---
+++ src/UserResolver.ts
@@ -0,0 +1,91 @@
+import { Arg, Ctx, Field, Mutation, ObjectType, Query, Resolver, UseMiddleware, Int } from 'type-graphql';
+import { compare, hash } from 'bcryptjs';
+import { getConnection } from 'typeorm';
+
+import { User } from './entity/User';
+import { MyContext } from './MyContext';
+import { createAccessToken, createRefreshToken } from './auth';
+import { isAuth } from './isAuth';
+import { sendRefreshToken } from './sendRefreshToken';
+
+@ObjectType()
+class LoginResponse {
+  @Field()
+  accessToken: string
+}
+
+@Resolver()
+export class UserResolver {
+  @Query(() => String)
+  hello() {
+    return 'hi!';
+  }
+
+  @Query(() => String)
+  @UseMiddleware(isAuth)
+  bye(
+    @Ctx() {payload}:MyContext
+  ) {
+    return `your user id is ${payload!.userId}`;
+  }
+
+  @Query(() => [User])
+  users() {
+    return User.find();
+  }
+
+  @Mutation(() => Boolean)
+  async revokeRefreshTokensForUser(
+    @Arg('userId', () => Int) userId: number
+  ) {
+    await getConnection()
+      .getRepository(User)
+      .increment({ id: userId }, 'tokenVersion', 1);
+
+    return true;
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg('email') email: string,
+    @Arg('password') password: string,
+    @Ctx() { res}: MyContext
+  ): Promise<LoginResponse> {
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    const valid = await compare(password, user.password);
+
+    if (!valid) {
+      throw new Error('Incorrect password');
+    }
+
+    sendRefreshToken(res, createRefreshToken(user));
+
+    return {
+      accessToken: createAccessToken(user)
+    };
+  }
+
+  @Mutation(() => Boolean)
+  async register(
+    @Arg('email') email: string,
+    @Arg('password') password: string
+  ) {
+    const hashedPassword = await hash(password, 12);
+
+    try {
+      await User.insert({
+        email,
+        password: hashedPassword
+      });
+    } catch (e) {
+      return false;
+    }
+
+    return true;
+  }
+}
